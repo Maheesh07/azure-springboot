153799b267285812f8a5ce6bba012deee5839fe0
Maheesh07 <69005323+Maheesh07@users.noreply.github.com>
2022-09-22 15:45:37 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GithubCicdActionsApplication {
 
-	@GetMapping("/welcome")
+	
 	public String welcome(){
 		return "Welcome to javatechie !";
 	}
